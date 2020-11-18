ff3415cd1ca9b6f25d2391f4009badd04fd8490d
pleiterson <pleiterson@gmail.com>
2020-11-18 10:32:20 -0300

    update links index footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,14 +7,14 @@ function Footer() {
       <p>
         Projeto desenvolvido na Imersão React da
         {' '}
-        <a target="_blank" href="https://www.alura.com.br/">
+        <a target="_blank" href="https://www.alura.com.br/" rel="noopener noreferrer">
           Alura
         </a>
       </p>
       <p>
         Repositório
         {' '}
-        <a target="_blank" href="https://github.com/Pleiterson/imersao-reactjs-pleiflix">
+        <a target="_blank" href="https://github.com/Pleiterson/imersao-reactjs-pleiflix" rel="noopener noreferrer">
           GitHub
         </a>
         {' '}
@@ -23,7 +23,7 @@ function Footer() {
       <p>
         Desenvolvido por
         {' '}
-        <a target="_blank" href="https://pleiterson.vercel.app">
+        <a target="_blank" href="https://pleiterson.vercel.app" rel="noopener noreferrer">
           Pleiterson Amorim
         </a>
       </p>
